77f2f3821f857568ed7d2cac7edd4851654e5502
LavishT <lavishtalwar28@github.com>
2020-06-17 10:48:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fushion2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
