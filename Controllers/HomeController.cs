db9ac9a51382f93e3bdf3e7f81866c8c8ddf0739
LavishT <lavishtalwar28@github.com>
2020-06-17 10:54:50 -0400

    Added About Us Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Fushion2.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
